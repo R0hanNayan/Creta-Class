abf4a32ff50fb80a09783716dc26e3880c756e7a
Rohan Nayan <rohannayan405@gmail.com>
2024-06-19 14:49:45 +0530

    Added About Section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './about.css';
 import infos from './index.js';
 import Card from '../../utils/Card/Card.jsx';
+import about2 from '../../assets/about2.png';
 
 const About = () => {
   return (
@@ -22,6 +23,18 @@ const About = () => {
                         )
                     })
                 }
+            </div>
+        </div>
+        <div className='about-2'>
+            <h1 className='about-header1'>Cultivate thinking skills from all aspects</h1>
+            <p className='about-p1'>Our expert advisors can help you find the right workplace solution for you and your team</p>
+            <div className='about-levels'>
+                <p className='about-p2-1'>S2 <br /> 3-4 Years Old</p>
+                <p className='about-p2-2'>S3 <br /> 5 Years Old</p>
+                <p className='about-p2-3'>S4 <br /> 6 Years Old</p>
+            </div>
+            <div className='about-2-img'>
+                <img src={about2} alt="About" />
             </div>
         </div>
     </div>
